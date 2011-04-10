c8cfc581381d8c0e264a938cc9791be98b2f6648
Aaron Roberts <aarondawg14@yahoo.com>
2011-04-10 19:01:20 -0400

    Added fishing, and fighting, made a little messy again. Sling too

--- StarterBot.java
+++ StarterBot.java
@@ -13,9 +13,12 @@ import org.rsbot.script.ScriptManifest;
 import org.rsbot.script.methods.Game;
 import org.rsbot.script.methods.Magic;
 import org.rsbot.script.methods.Skills;
+import org.rsbot.script.wrappers.RSGroundItem;
 import org.rsbot.script.wrappers.RSNPC;
 import org.rsbot.script.wrappers.RSObject;
+import org.rsbot.script.wrappers.RSPath;
 import org.rsbot.script.wrappers.RSTile;
+import org.rsbot.script.wrappers.RSTilePath;
 
 @ScriptManifest(authors = { "Camaro`", "Bentzilla" }, name = "StarterBot", keywords = "Woodcutting", description = "AIO AIO?", version = 1.337)
 public class StarterBot extends Script implements MouseListener,
@@ -56,7 +59,10 @@ public class StarterBot extends Script implements MouseListener,
 			new RSTile(2997, 3207), new RSTile(2996, 3201) };
 
 	public final int[] equipment = { 1351, 1349, 1353, 1361, 1355, 1357, 1359,
-			4031, 6739, 13470, 14108, 303, 590};//303 = net, 590 = tinderbox, rest = hatchets.
+			4031, 6739, 13470, 14108, 303, 590, 19830 };// 303 = net, 590 =
+														// tinderbox,
+														// rest = hatchets.
+														// 19830 = sling
 
 	public int[] trees = { 1276, 1278, 38788, 38782, 38787, 38783, 38786,
 			38784, 38760 };
@@ -69,7 +75,7 @@ public class StarterBot extends Script implements MouseListener,
 			startExp;
 	public int logsCut, oaksCut, willowsCut, bans, loads;
 	public Point mousePoint;
-	public boolean showProggy;
+	public boolean showProggy, pickedSling, stopSling;
 	public RSTile lumbyTile = new RSTile(3223, 3233);
 
 	public enum skillState {
@@ -79,6 +85,7 @@ public class StarterBot extends Script implements MouseListener,
 
 	public enum mainState {
 		CHOP, FISH, FIGHT, DROP, WAIT, RELOC;
+		// RELOC = Changing locations or getting back to the area
 	}
 
 	public enum fightState {
@@ -111,7 +118,13 @@ public class StarterBot extends Script implements MouseListener,
 			if (inventory.isFull()) {
 				return mainState.DROP;
 			}
-			if (calc.distanceTo(new RSTile(2988, 3178)) < 15) {
+			if ((calc.distanceTo(new RSTile(2986, 3181)) < 15)
+					|| (calc.distanceTo(new RSTile(2997, 3157)) < 15)) {
+
+				RSNPC f = npcs.getNearest(325);
+				if (f == null || !f.isOnScreen()) {
+					return mainState.RELOC;
+				}
 				if (getMyPlayer().isIdle()) {
 					return mainState.FISH;
 				} else {
@@ -125,7 +138,17 @@ public class StarterBot extends Script implements MouseListener,
 				return mainState.DROP;
 			}
 			if (calc.distanceTo(new RSTile(3000, 3205)) < 15) {
-				if (getMyPlayer().isIdle()) {
+				if (getMyPlayer().getHPPercent() < 35) {
+					while (getMyPlayer().isInCombat()) {
+						sleep(random(100, 500));
+						
+					}
+					log("Chopping until our HP is higher.");//TODO: switch to chop dont work
+					
+					return mainState.CHOP;
+				}
+				if (getMyPlayer().isIdle() && !getMyPlayer().isInCombat() && !getMyPlayer().isMoving()) {
+			
 					return mainState.FIGHT;
 				} else {
 					return mainState.WAIT;
@@ -151,7 +174,7 @@ public class StarterBot extends Script implements MouseListener,
 		if (skills.getRealLevel(Skills.WOODCUTTING) < 30) {
 			return skillState.WC;
 		}
-		if (skills.getRealLevel(Skills.FISHING) < 30) {
+		if (skills.getRealLevel(Skills.FISHING) < 5) {
 			return skillState.FISH;
 		}
 		return skillState.FIGHT;
@@ -159,11 +182,17 @@ public class StarterBot extends Script implements MouseListener,
 	}
 
 	public fightState getFightStyle() {
+		if (skills.getRealLevel(Skills.DEFENSE) < 30) {
+			return fightState.DEFENSE;
+		}
 		if (skills.getRealLevel(Skills.ATTACK) < 30) {
 			return fightState.ATTACK;
 		}
-		if (skills.getRealLevel(Skills.DEFENSE) < 30) {
-			return fightState.DEFENSE;
+		if (skills.getRealLevel(Skills.STRENGTH) < 30) {
+			return fightState.STRENGTH;
+		}
+		if (skills.getRealLevel(Skills.RANGE) < 30) {
+			return fightState.RANGE;
 		}
 		return fightState.STRENGTH;
 
@@ -245,6 +274,10 @@ public class StarterBot extends Script implements MouseListener,
 				currentLevel = skills.getCurrentLevel(Skills.DEFENSE);
 				s = "Defense";
 				break;
+			case RANGE:
+				currentLevel = skills.getCurrentLevel(Skills.RANGE);
+				s = "Range";
+				break;
 			}
 			break;
 
@@ -258,7 +291,7 @@ public class StarterBot extends Script implements MouseListener,
 			millis -= minutes * 1000 * 60;
 			long seconds = millis / 1000;
 
-			//int runTime = (int) (System.currentTimeMillis() - startTime);
+			// int runTime = (int) (System.currentTimeMillis() - startTime);
 
 			if (!showProggy) {
 				Composite c = AlphaComposite.getInstance(
@@ -276,17 +309,26 @@ public class StarterBot extends Script implements MouseListener,
 			g.drawString("Training: " + s, 563, 243);// 16
 			g.drawString(
 					"WC LVL: " + skills.getCurrentLevel(Skills.WOODCUTTING)
-							+ "/30" + " | " +skills.getExpToNextLevel(Skills.WOODCUTTING)+" XP / Lvl", 563, 259);
+							+ "/30" + " | "
+							+ skills.getExpToNextLevel(Skills.WOODCUTTING)
+							+ " XP / Lvl", 563, 259);
 			g.drawString("FISH LVL: " + skills.getCurrentLevel(Skills.FISHING)
-					+ "/30" + " | " +skills.getExpToNextLevel(Skills.FISHING)+" XP / Lvl", 563, 275);
+					+ "/30" + " | " + skills.getExpToNextLevel(Skills.FISHING)
+					+ " XP / Lvl", 563, 275);
 			g.drawString("Fighting -", 563, 291);
 
 			g.drawString("ATT LVL: " + skills.getCurrentLevel(Skills.ATTACK)
-					+ "/30" + " | " +skills.getExpToNextLevel(Skills.ATTACK)+" XP / Lvl", 573, 307);
+					+ "/30" + " | " + skills.getExpToNextLevel(Skills.ATTACK)
+					+ " XP / Lvl", 573, 307);
 			g.drawString("STR LVL: " + skills.getCurrentLevel(Skills.STRENGTH)
-					+ "/30" + " | " +skills.getExpToNextLevel(Skills.STRENGTH)+" XP / Lvl", 573, 323);
+					+ "/30" + " | " + skills.getExpToNextLevel(Skills.STRENGTH)
+					+ " XP / Lvl", 573, 323);
 			g.drawString("DEF LVL: " + skills.getCurrentLevel(Skills.DEFENSE)
-					+ "/30" + " | " +skills.getExpToNextLevel(Skills.DEFENSE)+" XP / Lvl", 573, 338);
+					+ "/30" + " | " + skills.getExpToNextLevel(Skills.DEFENSE)
+					+ " XP / Lvl", 573, 338);
+			g.drawString("RNG LVL: " + skills.getCurrentLevel(Skills.RANGE)
+					+ "/30" + " | " + skills.getExpToNextLevel(Skills.RANGE)
+					+ " XP / Lvl", 573, 354);
 
 		}
 
@@ -335,7 +377,7 @@ public class StarterBot extends Script implements MouseListener,
 				if (!walking.isRunEnabled()) {
 					walking.setRun(true);
 				}
-				sleep(random(1000, 3000));
+				sleep(random(2000, 4000));
 			}
 		}
 		return false;
@@ -379,7 +421,7 @@ public class StarterBot extends Script implements MouseListener,
 				tree.doAction("Chop");
 			}
 		}
-		return random(200, 400);
+		return random(400, 700);
 	}
 
 	public int fish() {
@@ -401,11 +443,83 @@ public class StarterBot extends Script implements MouseListener,
 				spot.doAction("Net");
 			}
 		}
-		return random(200, 400);
+		return random(400, 700);
 	}
 
 	public int fight() {
-		return random(200, 400);
+		switch (getFightStyle()) {
+		case DEFENSE:
+			combat.setFightMode(3);
+			break;
+		case ATTACK:
+			combat.setFightMode(1);
+			break;
+		case STRENGTH:
+			combat.setFightMode(2);
+			break;
+		case RANGE:
+			if (pickedSling) {
+				if (!stopSling) {
+					if (inventory.getItem(19830).doAction("ield")) {
+						sleep(random(1500, 2000));
+						combat.setFightMode(1);
+						stopSling = true;
+					}
+				}
+			} else {
+				if (random(0, 5) == 1) {
+					log("Waiting for a sling..");
+				}
+			}
+			break;
+		}
+		
+		RSGroundItem g = groundItems.getNearest(19830);
+		if (g != null && !pickedSling) {
+			if (g.isOnScreen()) {
+				g.doAction("Take");
+				while (getMyPlayer().isMoving()) {
+					sleep(50);
+				}
+				pickedSling = true;
+
+			} else {
+				walking.walkTileMM(g.getLocation());
+				RSGroundItem gg = groundItems.getNearest(19830);
+				if (gg != null) {
+					g.doAction("Take");
+					while (getMyPlayer().isMoving()) {
+						sleep(50);
+					}
+					pickedSling = true;
+				}
+			}
+		}
+		combat.setAutoRetaliate(true);
+		if (getMyPlayer().getHPPercent() < 35) {
+			return random(200, 400);
+		}
+		RSNPC spot = null;
+		if (getMyPlayer().getCombatLevel() > 17) {
+			spot = npcs.getNearest("Goblin", "Giant rat");
+		} else {
+			spot = npcs.getNearest("Goblin");
+		}
+		if (spot != null) {
+			if (!spot.isOnScreen()) {
+				camera.turnTo(spot);
+				if (!spot.isOnScreen()) {
+					walking.walkTileMM(spot.getLocation(), 1, 1);
+					return random(400, 700);
+				} else {
+					spot.doAction("Attack");
+				}
+
+			} else {
+				spot.doAction("Attack");
+			}
+		}
+		return random(400, 700);
 	}
 
 	@SuppressWarnings("deprecation")
@@ -424,6 +538,7 @@ public class StarterBot extends Script implements MouseListener,
 		log("Get Main = " + getMainState().toString());
 		if (getSkill().equals(skillState.RELOCATE)) {
 			while (calc.distanceTo(new RSTile(2996, 3201)) > 10) {
+				sleep(10);
 				walkPath(path);
 			}
 			return random(200, 400);
@@ -447,18 +562,45 @@ public class StarterBot extends Script implements MouseListener,
 			switch (getSkill()) {
 			case WC:
 				while (calc.distanceTo(new RSTile(2996, 3201)) > 10) {
-					walkPath(walking.findPath(new RSTile(2996, 3201)));//Logic works fine but this is failing?
-					//TODO: Fix this?
+					RSTile[] pp = walking.findPath(new RSTile(2996, 3201));
+					sleep(10);
+					walkPath(pp);
 				}
 				return random(200, 400);
 			case FISH:
-				while (calc.distanceTo(new RSTile(2996, 3201)) > 10) {
-					walkPath(walking.findPath(new RSTile(2996, 3201)));//Logic works fine but this is failing?
+				if (calc.distanceTo(new RSTile(2997, 3157)) < 9) {
+					// if at spot 2
+					while (calc.distanceTo(new RSTile(2986, 3181)) > 8) {
+						// while not at spot 2
+						RSTile[] pp = walking.findPath(new RSTile(2986, 3181));
+						sleep(10);
+						walkPath(pp);
+
+					}
+					return random(200, 400);
+				}
+				if (calc.distanceTo(new RSTile(2986, 3181)) < 9) {
+					// if at spot 1
+					while (calc.distanceTo(new RSTile(2997, 3157)) > 8) {
+						// while not at spot 2
+						RSTile[] pp = walking.findPath(new RSTile(2997, 3157));
+						sleep(10);
+						walkPath(pp);
+
+					}
+					return random(200, 400);
+				}
+
+				while (calc.distanceTo(new RSTile(2986, 3181)) > 10) {
+					RSTile[] pp = walking.findPath(new RSTile(2986, 3181));
+					sleep(10);
+					walkPath(pp);
 				}
 				return random(200, 400);
 			case FIGHT:
 				while (calc.distanceTo(new RSTile(2996, 3201)) > 10) {
-					walkPath(walking.findPath(new RSTile(2996, 3201)));//Logic works fine but this is failing?
+					walkPath(walking.findPath(new RSTile(2996, 3201)));
+					sleep(10);
 				}
 				return random(200, 400);
 			}
@@ -527,6 +669,5 @@ public class StarterBot extends Script implements MouseListener,
 	public void mouseReleased(MouseEvent arg0) {
 
 	}
-
 
 }
